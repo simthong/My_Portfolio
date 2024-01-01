af62681d88a6af3ec6a56fe341c621fb96a380ac
Victor David <davidsimthongz148@gmail.com>
2024-01-01 01:33:15 +0100

    Happy New Year

---
+++ My Portfolio/index.js
@@ -0,0 +1,13 @@
+// index.js
+
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
